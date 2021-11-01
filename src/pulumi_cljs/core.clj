13fb650359e13f2ac06dec73051f2f23c411bb53
Burin Choomnuan <burin.choomnuan@gmail.com>
2021-11-01 10:42:18 -0400

    Keep the indentation consistent

--- src/pulumi_cljs/core.clj
+++ src/pulumi_cljs/core.clj
@@ -10,8 +10,8 @@
   functions."
   [bindings & body]
   (let [binding-pairs (partition 2 bindings)
-       binding-forms (map first binding-pairs)
-       output-exprs (map second binding-pairs)]
+        binding-forms (map first binding-pairs)
+        output-exprs (map second binding-pairs)]
     `(.apply
        (all* (cljs.core/clj->js [~@output-exprs]))
        (fn [[~@binding-forms]]
